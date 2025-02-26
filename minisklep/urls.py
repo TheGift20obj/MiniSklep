cf105fcf3fffe4095088442bc68b3a822f270b36
TheGift20obj <=>
2025-02-26 12:48:42 +0100

    Added functionality for adding, displaying, and editing products

--- minisklep/urls.py
+++ minisklep/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from sklep.views import lista_produktow
+from sklep import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', lista_produktow, name='lista_produktow')
+    path('', views.lista_produktow, name='lista_produktow'),  # Lista produktów
+    path('dodaj/', views.dodaj_edycja_produkt, name='dodaj_edycja_produkt'),  # Dodaj nowy produkt
+    path('edycja/<int:produkt_id>/', views.dodaj_edycja_produkt, name='dodaj_edycja_produkt'),  # Edytuj produkt
+    path('usun/<int:produkt_id>/', views.usun_produkt, name='usun_produkt'),  # Usuń produkt
+    path('usun_wybrane/', views.usun_wybrane_produkty, name='usun_wybrane_produkty'),
 ]
--- sklep/views.py
+++ sklep/views.py
@@ -1,6 +1,36 @@
 from django.shortcuts import render
 from .models import Produkt
+from .forms import ProduktForm
 
 def lista_produktow(request):
     produkty = Produkt.objects.all()
     return render(request, '../templates/lista_produktow.html', {'produkty': produkty})
+
+def dodaj_edycja_produkt(request, produkt_id=None):
+    if produkt_id:
+        produkt = get_object_or_404(Produkt, id=produkt_id)
+    else:
+        produkt = None
+
+    if request.method == 'POST':
+        form = ProduktForm(request.POST, request.FILES, instance=produkt)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_produktow')  # Zmienna 'lista_produktow' to widok, który wyświetla wszystkie produkty
+    else:
+        form = ProduktForm(instance=produkt)
+
+    return render(request, '../templates/dodaj_edycja_produkt.html', {'form': form, 'produkt': produkt})
+
+def usun_wybrane_produkty(request):
+    if request.method == 'POST':
+        produkt_ids = request.POST.getlist('produkty_do_usuniecia')
+        for produkt_id in produkt_ids:
+            produkt = get_object_or_404(Produkt, id=produkt_id)
+            produkt.delete()
+    return redirect('lista_produktow')
+
+def usun_produkt(request, produkt_id):
+    produkt = get_object_or_404(Produkt, id=produkt_id)
+    produkt.delete()
+    return redirect('lista_produktow')
