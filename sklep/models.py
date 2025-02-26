a2888fe85ccba15950b5822fac5765f09bac9f01
TheGift20obj <=>
2025-02-26 12:27:52 +0100

    Added superuser

--- sklep/models.py
+++ sklep/models.py
@@ -1,10 +1,1 @@
 from django.db import models
-
-class Produkt(models.Model):
-    nazwa = models.CharField(max_length=100)
-    opis = models.TextField()
-    cena = models.DecimalField(max_digits=10, decimal_places=2)
-    zdjecie = models.ImageField(upload_to='produkty/', null=True, blank=True)
-
-    def __str__(self):
-        return self.nazwa
--- sklep/views.py
+++ sklep/views.py
@@ -1,6 +1,1 @@
 from django.shortcuts import render
-from .models import Produkt
-
-def lista_produktow(request):
-    produkty = Produkt.objects.all()
-    return render(request, 'sklep/lista_produktow.html', {'produkty': produkty})
