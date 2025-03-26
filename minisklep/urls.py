c23d00079a1e97943a3feb809f8be9ba6e022fca
TheGift20obj <=>
2025-03-26 11:48:19 +0100

    Dodane: przycisk anuluj do dodai i edytuj, funkcjonalnosc logowania i wylogawania, lepsza zawartosc strony

--- minisklep/urls.py
+++ minisklep/urls.py
@@ -5,6 +5,8 @@ from sklep import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.lista_produktow, name='lista_produktow'),  # Lista produktów
+    path('login/', views.login_view, name='login'),  # Logowanie
+    path('logout/', views.logout_view, name='logout'),
     path('dodaj/', views.dodaj_edycja_produkt, name='dodaj_edycja_produkt'),  # Dodaj nowy produkt
     path('edycja/<int:produkt_id>/', views.dodaj_edycja_produkt, name='dodaj_edycja_produkt'),  # Edytuj produkt
     path('usun/<int:produkt_id>/', views.usun_produkt, name='usun_produkt'),  # Usuń produkt
--- sklep/views.py
+++ sklep/views.py
@@ -1,11 +1,33 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 from .models import Produkt
 from .forms import ProduktForm
 
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            auth_login(request, user)  # Poprawnie logujemy użytkownika
+            return redirect('lista_produktow')  # Przekierowanie po udanym logowaniu
+        else:
+            # Dodajemy tutaj jakiś komunikat błędu, jeśli uwierzytelnienie się nie uda
+            return render(request, 'login.html', {'error': 'Niepoprawny login lub hasło'})
+    return render(request, 'login.html')
+
+
+def logout_view(request):
+    auth_logout(request)  # Poprawnie wylogowujemy użytkownika
+    return redirect('login')  # Możesz przekierować na stronę logowania po wylogowaniu
+
+@login_required(login_url='login')
 def lista_produktow(request):
     produkty = Produkt.objects.all()
     return render(request, '../templates/lista_produktow.html', {'produkty': produkty})
 
+@login_required(login_url='login')
 def dodaj_edycja_produkt(request, produkt_id=None):
     if produkt_id:
         produkt = get_object_or_404(Produkt, id=produkt_id)
@@ -22,6 +44,7 @@ def dodaj_edycja_produkt(request, produkt_id=None):
 
     return render(request, '../templates/dodaj_edycja_produkt.html', {'form': form, 'produkt': produkt})
 
+@login_required(login_url='login')
 def usun_wybrane_produkty(request):
     if request.method == 'POST':
         produkt_ids = request.POST.getlist('produkty_do_usuniecia')
@@ -30,6 +53,7 @@ def usun_wybrane_produkty(request):
             produkt.delete()
     return redirect('lista_produktow')
 
+@login_required(login_url='login')
 def usun_produkt(request, produkt_id):
     produkt = get_object_or_404(Produkt, id=produkt_id)
     produkt.delete()
